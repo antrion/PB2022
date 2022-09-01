0d2c56218f66131582dd4c80defe7ad940e5d716
AGUSTIN FREIRE <agustinfreire@hotmail.com>
2022-09-01 17:25:24 -0300

    Servidor con Express

--- main.js
+++ main.js
@@ -1,75 +1,43 @@
-const fs = require("fs");
+const express = require("express");
+const db = require("./contenedor.js");
+const app = express();
 
-class Contenedor {
+// middleware https://expressjs.com/es/api.html#express.urlencoded
+app.use(express.urlencoded());
+// middleware https://expressjs.com/es/api.html#express.json
+app.use(express.json());
+//* request/ response
+const DB = new db("data");
 
-    constructor(archivo) {
-        this.archivo = archivo;
-    }
+// root = no hay problema
+app.get("/", (req, res) => {
+    res.send({ error: false });
+});
 
-    async save(objProd) {
-        // * Se lee el archivo para obtener la cantidad de productos y generar un nuevo id
-        const data = await fs.promises.readFile(`${this.archivo}/productos.json`,"utf-8");
-        const productos = JSON.parse(data);
-        const id = productos.length + 1;
-        objProd.id = id;
-        // * { name: string, price: int, thumbnail: string, id: id }
-        productos.push(objProd);
-        const productosString = JSON.stringify(productos);
-        await fs.promises.writeFile(`${this.archivo}/productos.json`,productosString);
-    
-        return id;
-    }
+//getAll
+app.get("/productos", async (req, res) => {
+    const data = await DB.getAll();
+    return res.send(data);
+});
 
-    async getById(id) {
-        const data = await fs.promises.readFile(`${this.archivo}/productos.json`,"utf-8");
-        const productos = JSON.parse(data);
-        const producto = productos.find((producto) => producto.id == id);
-        if (producto) {
-            return producto;
-        } else {
-            return "Producto no encontrado";
-        }
-    }
+//getById
+// queries
+// * GET ?id=10
 
-    async getAll() {
-        try {
-            const data = await fs.promises.readFile(`${this.archivo}/productos.json`,"utf-8");
-            return JSON.parse(data);
-        } catch (error) {
-            return [];
-        }
-    }
+app.get("/productosRandom", async (req, res) => {
+const data = await DB.getAll();
 
-    async deleteById(id) {
-        // * Se lee el archivo para obtener la cantidad de productos y generar un nuevo id
-        const data = await fs.promises.readFile(`${this.archivo}/productos.json`,"utf-8");
-        const productos = JSON.parse(data);
-        const productosFilter = productos.filter(item => item.id !== id)
-        const productosString = JSON.stringify(productosFilter);
-        await this.deleteAll();
-        await fs.promises.writeFile(`${this.archivo}/productos.json`,productosString);
-    
-        return id;
-    }
-
-    async deleteAll() {
-        await fs.promises.writeFile(`${this.archivo}/productos.json`,"[]");
-    }
+function numAleatorio(num) {
+    return parseInt(Math.random() * num + 1);
 }
 
-//TEST
+const size = Object.keys(data);
+const id = numAleatorio(size.length)
+const randomProd = await DB.getById(id);
 
-async function start() {
-    const db = new Contenedor("data");
-    db.save({ name: "Escoba Plastica", price: 287, thumbnail: "http://www.insumosactivos.com.ar/front/img/fotoProductos/archs/escobilla.jpg" });
-    const productos = await db.getAll();    
-    console.log(productos);
-    const producto = await db.getById(1);
-    console.log(producto);
-    await db.deleteAll();
-    console.log(await db.getAll());
-    //console.log(await db.deleteById(30));
-    //console.log(await db.getAll());
-}
+return res.send(randomProd);  
+});
 
-start();
+app.listen(8080, () => {
+    console.log("Iniciado");
+});
